b28ec1636a54726259a710aace9d92e0882cb7f7
Don Bigosso <w.bogdan@ymail.com>
2026-03-05 08:39:25 +0000

    backend rename method implemented

--- api_source/classes/file_model.php
+++ api_source/classes/file_model.php
@@ -13,9 +13,22 @@
             $user = new UserModel($this->db);
             $logged_user_verify = $user->get_by_token($token);
             if(!$logged_user_verify){
-                return false;
+                return ["renamed"=>false, "error"=>"User is not logged in."];
             }
-            return [$this->upload_folder, $old_filename, $new_filename, $logged_user_verify];
+            $filename_regEx = '/^[a-zA-Z0-9._\-\s]{5,50}$/';
+            $filename_test = preg_match($filename_regEx, $new_filename);
+            if(!$filename_test){
+                return ["renamed"=>false, "error"=>"Filename does not meet the requirements."];
+            }
+            $file_path = $this->upload_folder . '/' . $old_filename;
+            $file_exist  = file_exists($file_path);
+            if(!$file_exist){
+                return ["renamed"=>false, "error"=>"File does not exist."];
+            }
+            //rename file
+            $new_file_path = $this->upload_folder . '/' . $new_filename;
+            rename($file_path, $new_file_path);
+            return ["renamed"=>true, "error"=>""];
         }
     }
 ?>
